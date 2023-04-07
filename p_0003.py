eae47f271c092288b328d8c4167ae093fe5bfb1c
bavaaram <itsjustabavaar@gmail.com>
2023-04-07 06:43:02 +0330

    Add required codes for problem_0003.py Program.

---
+++ p_0003.py
@@ -0,0 +1,9 @@
+#Calculate the volume and area of the sphere by reading its radius.
+pi = 3.1415926
+radius = float(input("Enter Radius of cylinder: "))
+
+volume = 4 / 3 * (pi * (radius ** 2))
+area = 4 * pi * (radius ** 2)
+
+print("Volume is: %f" % volume)
+print("Area is: %f" % area)
