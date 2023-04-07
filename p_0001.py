4ad577e0f3a9d99ab0963d6c785407a1ac7ecde8
bavaaram <itsjustabavaar@gmail.com>
2023-04-07 06:28:18 +0330

    Add required codes to the problem_0001.py program.

---
+++ p_0001.py
@@ -0,0 +1,6 @@
+#Calculation of the area of a parallelogram with its height and base.
+height = float(input("Please Enter height: "))
+base = float(input("Please Enter base: "))
+area = height * base
+
+print("Area is %f" % area)
