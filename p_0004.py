ed96a39a81e76fda1dd94d8f0bc7073d3316de6d
bavaaram <itsjustabavaar@gmail.com>
2023-04-10 04:37:47 +0330

    Edit something in p_0004.py Program and "import math" changed to "from math import pi, tan" for better performance and dont import the whole math library.

--- p_0004.py
+++ p_0004.py
@@ -1,11 +1,10 @@
 #Reading the number of sides and the length of each side of a regular polygon and calculating its area.
 
-import math
+from math import tan, pi
 
 num_side = int(input("Enter number of sides: "))
 side_length = float(input("Enter length of each side: "))
-pi = 3.1415926
 
-area = ((num_side) * (side_length ** 2)) / (4 * math.tan((pi) / (num_side)))
+area = ((num_side) * (side_length ** 2)) / (4 * tan((pi) / (num_side)))
 
 print("The area of Regular polygon is %s" % str(area))
